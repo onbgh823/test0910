5edb31451491c17fc41dbfa24e214990314f8b82
onbgh823 <68522421+onbgh823@users.noreply.github.com>
2020-09-10 15:53:24 +0900

    Add files via upload

---
+++ common.js
@@ -0,0 +1,26 @@
+(function($){
+
+    
+    $('.depth1 > li').hover(
+        function(){
+            $(this).find('.depth2')
+            .stop().slideDown(700)
+        },
+        function(){
+            $(this).find('.depth2')
+            .stop().slideUp(500)
+        }
+    )
+
+
+        $('#kimBox').load('main.html')
+
+        $('.topmenu > a').on('click', function(e){
+            e.preventDefault()
+            var url = $(this).attr('href')
+            $('#kimContainer').remove()
+            $('#kimBox').load(url)
+        })
+
+
+})(jQuery)
